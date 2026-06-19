f406f0e595fc56ef4568ebd50803de53198820f7
Udit Kumar Chatterjee <quantumudit@gmail.com>
2026-06-20 02:26:00 +0530

    wip: 0001 two_sum -- add README, notes, clean solution vars

---
+++ problems/leetcode/easy/0001_two_sum/solution.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}
+        for i in range(len(nums)):
+            complement = target - nums[i]
+            if complement in seen:
+                return [i, seen[complement]]
+            seen[nums[i]] = i  # store index, not value
