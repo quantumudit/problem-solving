2a40c3467c30c20ba74cdb1465c767ff8250432a
Udit Kumar Chatterjee <quantumudit@gmail.com>
2026-06-27 19:31:17 +0530

    refactor: improve readability of top/bottom country selection in q01 and q02 scripts

--- projects/misc/demographic_data_analysis/scripts/q01_solution_pandas.py
+++ projects/misc/demographic_data_analysis/scripts/q01_solution_pandas.py
@@ -8,8 +8,8 @@ data_dir = Path(__file__).parent.parent / "data"
 
 df = pd.read_csv(data_dir / "demographic_data.csv")
 
-top_10_by_birth_rate = df.nlargest(10, "Birth Rate")[["Country Name", "Birth Rate"]]
-bottom_10_by_birth_rate = df.nsmallest(10, "Birth Rate")[["Country Name", "Birth Rate"]]
+top_10_by_birth_rate = df[["Country Name", "Birth Rate"]].nlargest(10, "Birth Rate")
+bottom_10_by_birth_rate = df[["Country Name", "Birth Rate"]].nsmallest(10, "Birth Rate")
 
 show(top_10_by_birth_rate, "Top 10 Countries by Birth Rate")
 show(bottom_10_by_birth_rate, "Bottom 10 Countries by Birth Rate")
--- projects/misc/demographic_data_analysis/scripts/q02_solution_pandas.py
+++ projects/misc/demographic_data_analysis/scripts/q02_solution_pandas.py
@@ -8,12 +8,12 @@ data_dir = Path(__file__).parent.parent / "data"
 
 df = pd.read_csv(data_dir / "demographic_data.csv")
 
-top_10_by_internet_users = df.nlargest(10, "Internet Users")[
-    ["Country Name", "Internet Users"]
-]
-bottom_10_by_internet_users = df.nsmallest(10, "Internet Users")[
-    ["Country Name", "Internet Users"]
-]
+top_10_by_internet_users = df[["Country Name", "Internet Users"]].nlargest(
+    10, "Internet Users"
+)
+bottom_10_by_internet_users = df[["Country Name", "Internet Users"]].nsmallest(
+    10, "Internet Users"
+)
 
 show(top_10_by_internet_users, "Top 10 Countries by Internet Users")
 show(bottom_10_by_internet_users, "Bottom 10 Countries by Internet Users")
