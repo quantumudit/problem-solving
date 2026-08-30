77e495feef6222a01f7ac8e4e47fcdb61cb2b49b
Udit Kumar Chatterjee <quantumudit@gmail.com>
2026-08-30 16:05:44 +0530

    wip: solve q17-q18 india questions

--- projects/kaggle/olympic_history_analysis/scripts/05_india/q18_solution.sql
+++ projects/kaggle/olympic_history_analysis/scripts/05_india/q18_solution.sql
@@ -1,1 +1,12 @@
 -- In which Olympic Games did India win medals in Hockey, and how many?
+
+SELECT
+    ae.Games AS olympic_game,
+    COUNT(ae.Medal) AS total_medals
+FROM athlete_events AS ae
+JOIN noc_regions AS nr USING (NOC)
+WHERE ae.Medal != 'NA'
+    AND nr.region = 'India'
+    AND ae.Sport = 'Hockey'
+GROUP BY ae.Games
+ORDER BY ae.Games;
