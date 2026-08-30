f27f01037e38efb85d9059d7d0eacfb23848845f
Udit Kumar Chatterjee <quantumudit@gmail.com>
2026-08-30 13:26:00 +0530

    wip: solve q06-q08 sports questions

--- projects/kaggle/olympic_history_analysis/scripts/02_sports/q06_solution.sql
+++ projects/kaggle/olympic_history_analysis/scripts/02_sports/q06_solution.sql
@@ -1,1 +1,10 @@
 -- Which sports have featured in every Summer Olympics?
+
+SELECT
+    Sport,
+    COUNT(DISTINCT Games) AS summer_games_appeared
+FROM athlete_events
+WHERE Season = 'Summer'
+GROUP BY Sport
+HAVING COUNT(DISTINCT Games) = (SELECT COUNT(DISTINCT Games) FROM athlete_events WHERE Season = 'Summer')
+ORDER BY Sport;
--- projects/kaggle/olympic_history_analysis/scripts/02_sports/q08_solution.sql
+++ projects/kaggle/olympic_history_analysis/scripts/02_sports/q08_solution.sql
@@ -1,1 +1,8 @@
 -- How many sports were played in each Olympic Game?
+
+SELECT
+    Games,
+    COUNT(DISTINCT Sport) AS sports_played
+FROM athlete_events
+GROUP BY Games
+ORDER BY Games;
